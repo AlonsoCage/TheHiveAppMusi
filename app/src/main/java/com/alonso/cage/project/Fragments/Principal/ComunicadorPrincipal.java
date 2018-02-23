a991c7c491444a07756c3732b1c68a76a9bf4789
alonso <alonso@hotmail.com>
2018-02-23 12:32:06 +0100

    Initial commit

---
+++ app/src/main/java/com/alonso/cage/project/Fragments/Principal/ComunicadorPrincipal.java
@@ -0,0 +1,12 @@
+package com.alonso.cage.project.Fragments.Principal;
+
+/**
+ * Created by user on 11/01/2018.
+ */
+
+public interface ComunicadorPrincipal {
+    void actualizarPost();
+    void finalizar();
+    void hideKeyboard();
+    void actualizarFragmentAudio();
+}
---
+++ app/src/main/java/com/alonso/cage/project/Fragments/Principal/UploadAudio/ComunicadorAudio.java
@@ -0,0 +1,9 @@
+package com.alonso.cage.project.Fragments.Principal.UploadAudio;
+
+/**
+ * Created by user on 11/01/2018.
+ */
+
+public interface ComunicadorAudio {
+    void actualizar();
+}
